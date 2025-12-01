d172ae44df8c62c5a92a325762375cd8b65a751c
Abdou Hidoussi <hidoussiabdou5@gmail.com>
2025-12-01 09:09:35 +0100

    teams

--- flask/app.py
+++ flask/app.py
@@ -15,10 +15,37 @@ client = MongoClient(host='mongodb://devinov:Devinov2022$@162.19.233.48:27017/?a
 db  = client["Arbi_Qr"] # replace with your DB name
 collection = db['Scans']  # replace with your collection name
 
+users = {
+    "user 1": "password123",
+    "user 2": "mypassword123",
+    "user 3": "password321",
+    "user 4": "mypassword321",
+    "user 5": "pass123word"
+}
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Missing JSON body"}), 400
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "username and password required"}), 400
+
+    # Check credentials
+    if username in users and users[username] == password:
+        return jsonify({"message": "Login successful"}), 200
+
+    return jsonify({"error": "Invalid credentials"}), 401
+
 @app.route('/')
 def map_view():
     # Fetch all documents with username, time, and location
-    data = list(collection.find({}, {'_id': 0, 'username': 1, 'time': 1, 'location': 1}))
+    data = list(collection.find({}, {'_id': 0, 'username': 1, 'time': 1, 'location': 1, 'qr_data': 1}))
     # Expect location: {'lat': ..., 'lng': ...}
     return render_template('map.html', data=data)
 
@@ -30,7 +57,7 @@ def save_scan():
     if not data:
         return jsonify({'error': 'No data provided'}), 400
 
-
+    username = data.get('username')
     qr_data = data.get('qr_data')
     location = data.get('location')
     timestamp = data.get('timestamp')
@@ -50,7 +77,8 @@ def save_scan():
 
     scan_doc = {
 
-        'username': qr_data,
+        'username': username,
+        'qr_data': qr_data,
         'location': location,
         'time': timestamp_dt
 
